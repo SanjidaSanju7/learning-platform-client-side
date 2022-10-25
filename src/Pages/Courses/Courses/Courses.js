5a6e7bdeb51c79325f8e4c87060b8de029884e11
SanjidaSanju7 <sanjidaakter6789@gmail.com>
2022-10-25 19:46:57 +0600

    courses list added

--- src/Pages/Courses/Courses/Courses.js
+++ src/Pages/Courses/Courses/Courses.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import LeftSideBar from '../../Shared/LeftSideBar/LeftSideBar';
 
 const Courses = () => {
+
+
     return (
-        <div>
-            <h1>This is Courses Page</h1>
-        </div>
+        <div className='mt-8'>
+            <div class="flex lg:flex-row ">
+                <div class="basis-3/12 text-center font-semibold p-5">
+                    <LeftSideBar></LeftSideBar>
+                </div>
+
+                <div class="basis-9/12">
+                    Cards
+                </div>
+            </div>
+        </div >
     );
 };
 
---
+++ src/Pages/Shared/LeftSideBar/LeftSideBar.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const LeftSideBar = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/course-categories')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, []);
+
+    return (
+
+        <div>
+            <h2 className='title text-3xl'>All Courses Name</h2>
+            <div >
+                {
+                    categories.map(category => <p className='py-2 text-start ml-5' key={category.id}>
+                        <Link>{category.name}</Link>
+                    </p>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default LeftSideBar;
